28c70e8f52644e792b813a1d545d1fb8ded7bd2b
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2014-06-16 01:12:00 +0800

    Keterangan pada Controller...

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,19 +1,46 @@
 <?php
 
+/**
+ * namespace digunakan untuk menyatakan 
+ * lokasi directory / folder 
+ * tempat file ini berada.
+ * 
+ * Cara penulisan namespace adalah 
+ * dengan menuliskan nama directory / folder
+ * diawali dengan huruf besar
+ * seperti pada sintak dibawah ini.
+ */
+
 namespace App\Controllers;
 
+/**
+ * "use" merupakan sebuah sintak
+ * yang digunakan untuk memanggil
+ * sebuah class yang digunakan dalam
+ * file class ini (BaseController)
+ */
 use App\Controllers\BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 
 /**
- * Description of AdminController
+ * AdminController adalah sebuah Controller
+ * yang digunakan melakukan filter terhadap
+ * autentikasi user.
+ * 
+ * Jika user belum melakukan login maka
+ * AdminController akan mengarahkan ke halaman
+ * login.
  *
  * @author aljufry
  */
 class AdminController extends BaseController {
 
     function __construct() {
+        
+        /**
+         * 
+         */
         $this->beforeFilter('auth', array('except' => '/login'));
     }
 
--- app/helpers/Printpre.php
+++ app/helpers/Printpre.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Printpre
  *
@@ -13,6 +7,15 @@
  */
 class PrintPre {
 
+    /**
+     * Method ini digunakan untuk mencetak
+     * array, object dan berbagai macam
+     * tipe data dalam struktrur yang rapi
+     * menggunakan tag pre html
+     * 
+     * @param mix $var
+     * @param boolean $detail
+     */
     public static function out($var, $detail = false) {
         if ($detail == false) {
             echo "<pre>";
